6046c4974ad90bcbf9767aca21aebacf556a8f03
Emma <emsca2002@gmail.com>
2023-06-12 15:38:07 -0400

    lots of things!

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,27 +1,27 @@
-function App() {
-    const title = "slayer title"
-    const body = "boop boop beep beep boop"
-    const comments = [
-        {id: 1, text: "comment one"},
-        {id: 2, text: "comment two"},
-        {id: 3, text: "comment three"},
-    ]
+import { useState } from "react"
+import NewHeader from "./components/NewHeader"
+import FeedbackList from "./components/FeedbackList"
+import FeedbackData from "./data/FeedbackData"
+import FeedbackStats from "./components/FeedbackStats"
+import FeedbackForm from "./components/FeedbackForm"
 
+function App() {
+    const [feedback, setFeedback] = useState(FeedbackData)
+
+    const deleteFeedback = (id) => {
+        if (window.confirm('Are you sure you want to delete this feedback?')) {
+            setFeedback(feedback.filter((item) => item.id !== id))
+        }
+    }
     return (
-        <div className="container">
-            <h1>{title.toUpperCase()}</h1>
-            <p>{body}</p>
-            <div>
-                <h3>
-                    Comments ({comments.length})
-                </h3>
-                <ul>
-                    {comments.map((comment, index) => (
-                        <li key={index}>{comment.text}</li>
-                    ))}
-                </ul>
+        <>
+            <NewHeader />
+            <div className="container">
+                <FeedbackForm/>
+                <FeedbackStats feedback={feedback}/>
+                <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
             </div>
-        </div>
+        </>
     )
 }
 
