ed7e47abf7c41c2d5d931b6786e5afff221e365b
Emma <emsca2002@gmail.com>
2023-06-14 15:29:58 -0400

    create context and remove unnecessary state

--- feedback-app/src/pages/AboutPage.jsx
+++ feedback-app/src/pages/AboutPage.jsx
@@ -1,14 +1,26 @@
+import { Link } from "react-router-dom"
 import Card from "../components/shared/Card"
+import { motion, AnimatePresence } from 'framer-motion'
 
 function AboutPage() {
     return (
-        <Card>
-            <div className="about">
-            <h1>About This Project</h1>
-            <p>This is a React app to leave feedback for a product or service.</p>
-            <p>Version: 1.0.0</p>
-            </div>
-        </Card>
+        <AnimatePresence>
+            <motion.div 
+                initial={{opacity: 0}}
+                animate={{opacity: 1}}
+                exit={{opacity: 0}}>
+                <Card>
+                    <div className="about">
+                        <h1>About This Project</h1>
+                        <p>This is a React app to leave feedback for a product or service.</p>
+                        <p className="regular-link">
+                            <Link to="/">Back to Home</Link>
+                        </p>
+                        <p className="version">v1.0.0</p>
+                    </div>
+                </Card>
+            </motion.div>
+        </AnimatePresence>
     )
 }
 
